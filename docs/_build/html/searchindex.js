fa9d66b05a1217cc6e1776f5ef55309a9a4a885c
Andy Tsai <andytsai@mit.edu>
2018-03-01 03:02:28 -0500

    deleted python docs, use wiki for now

--- docs/_build/html/searchindex.js
+++
@@ -1,1 +0,0 @@
-Search.setIndex({envversion:50,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{content:0,index:0,modul:0,page:0,search:0},titles:["Welcome to Factor Graph&#8217;s documentation!"],titleterms:{document:0,factor:0,graph:0,indice:0,tabl:0,welcom:0}})
