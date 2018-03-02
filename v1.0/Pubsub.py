43f164f0e2a4b5ae2d4d52f5cc7d1e1ad452ceab
Andy Tsai <andytsai14@gmail.com>
2018-03-02 15:58:16 -0500

    creates end to end skeleton of APIs with working test

--- v1.0/Pubsub.py
+++ v1.0/Pubsub.py
@@ -1,21 +1,24 @@
-class Pubsub: # how to map id to object?
+class PubSub: # how to map id to object?
     def __init__(self):
+
         self.publishers = dict()
         self.channels = dict()
         self.subscribers = dict()
         self.channel_to_subscriber = dict()
+    
+    '''
+    def register_publishers(self, list_of_publishers):
+        for publisher in list_of_publishers:
+            publisher.register()
 
-    def publisher_reg(self, publisher, id):
-        self.publishers[id] = publisher
-        #ligra.publisher(id)
+    def register_channels(self, list_of_channels):
+        for channel in list_of_channels:
+            channel.register()
 
-    def channel_reg(self, channel, id):
-        self.channels[id] = channel
-        #ligra.channel(id)
 
-    def subscriber_reg(self, subscriber, id):
-        self.subscribers[id] = subscriber
-        #ligra.subscriber(id)
+    def register_subscribers(self, list_of_subscribers):
+        for subscriber in list_of_subscribers:
+            subscriber.register()
 
     def subscribe(channel_id, subscriber_id):
         if channel_id in self.channel_to_subscriber:
@@ -24,15 +27,34 @@ class Pubsub: # how to map id to object?
             self.channel_to_subscriber[channel_id] = [subscriber_id]
 
         #ligra.subscribe(channel_id, subscriber_id)
+    '''
 
 class Publisher:
-    def __init__(self):
+    def __init__(self, node_id):
         pass
+
+    def publish(self):
+        pass
+
+    def register(self):
+        pass
+
+
 
 class Channel:
     def __init__(self):
         pass
 
-class Subscriber:
-    def __init__(self):
+    def register(self):
         pass
+
+
+class Subscriber:
+    def __init__(self, node_id):
+        self.callback_function = None
+
+    def subscribe(self):
+        pass
+
+    def register(self, callback_function):
+        self.callback_function = callback_function
