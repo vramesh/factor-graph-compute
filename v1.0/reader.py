9de5d61e8c5eac8ab6c202a97d626558ecca6b2a
Suchan Vivatsethachai <suchanv@mit.edu>
2018-03-09 15:23:20 -0500

    adds redis pubsub support

---
+++ v1.0/reader.py
@@ -0,0 +1,54 @@
+from node import Node
+from edge import Edge
+
+class FactorGraphReader:
+    def register_pubsub_from_pagerank_adjacency_list(path_to_input_file, pubsub, wrapper_var_function, wrapper_fac_function, factor_graph):
+        (adjacency_dict_var,adjacency_dict_fac) = FactorGraphReader.read_file_factor_graph(path_to_input_file) #{1:[2,3]}
+        num_node = len(adjacency_dict_var)
+
+        for variable_id in adjacency_dict_var:
+            initial_messages_var = dict(adjacency_dict_var[variable_id])
+            node_data = len(adjacency_dict_var[variable_id])-1
+            variable_node = Node(variable_id,"variable",wrapper_var_function,initial_messages_var,node_data,pubsub)
+            factor_graph.variable_nodes.append(variable_node)
+
+        for factor_id in adjacency_dict_fac:
+            initial_messages_fac = dict(adjacency_dict_fac[factor_id])
+            node_data = 0
+            factor_node = Node(factor_id,"factor",wrapper_fac_function,initial_messages_fac,node_data,pubsub)
+            factor_graph.factor_nodes.append(factor_node)
+
+        for variable_id in adjacency_dict_var:
+            for (factor_id,initial_message) in adjacency_dict_var[variable_id]:
+                channel_name = variable_id + "_" + factor_id
+                edge = Edge(variable_id,factor_id, channel_name, pubsub)
+                factor_graph.edges.append(edge)
+
+        for factor_id in adjacency_dict_fac:
+            for (variable_id,initial_message) in adjacency_dict_fac[factor_id]:
+                channel_name = factor_id + "_" + variable_id
+                edge = Edge(factor_id,variable_id, channel_name, pubsub)
+                factor_graph.edges.append(edge)
+
+        return factor_graph
+
+    def read_file_factor_graph(path_to_input_file): 
+        adjacency_dict_var = dict() #key: variable index, value: list of factor index
+        adjacency_dict_fac = dict()
+        with open(path_to_input_file) as f:
+            all_lines = f.readlines()
+            for line in all_lines:
+                [x,y,initial_incoming_message] = line.split()
+                initial_incoming_message = float(initial_incoming_message)
+
+                if y[0]=="v":
+                    add_to_adjacency_dict = adjacency_dict_var
+                elif y[0]=="f":
+                    add_to_adjacency_dict = adjacency_dict_fac
+
+                if y in add_to_adjacency_dict:
+                    add_to_adjacency_dict[y].append((x,initial_incoming_message))
+                else:
+                    add_to_adjacency_dict[y] = [(x,initial_incoming_message)]
+
+        return (adjacency_dict_var,adjacency_dict_fac)
---
+++ v1.0/redis_broker.py
@@ -0,0 +1,73 @@
+import redis
+import time
+from multiprocessing import Process, Manager, Array
+import pdb
+from threading import Thread
+
+
+class RedisBroker:
+    def __init__(self):
+        # self.redis_main = redis.StrictRedis(host='localhost',port=6379, db=0, decode_responses=True)
+        self.redis_main = redis.Redis()
+        self.redis_pubsub_object = self.redis_main.pubsub(ignore_subscribe_messages=True)
+        self.publishers = set()
+        self.channels = list()
+        self.subscribers = dict()
+
+    def add_publisher(self,publisher_id):
+        if publisher_id in self.publishers:
+            return "Need unique publisher id"
+        self.publishers.add(publisher_id)
+
+    def add_subscriber(self,subscriber_id, callback_function):
+        if subscriber_id in self.subscribers:
+            return "Need unique subscriber id"
+        new_subscriber = self.redis_pubsub_object
+        self.subscribers[subscriber_id] = {"redis_pubsub": new_subscriber,
+                                           "callback_function": callback_function}
+
+    def add_channel(self,channel_id):
+        self.channels.append(channel_id)
+
+    def add_subscription(self,subscriber_id, channel_id):
+        callback_function = self.subscribers[subscriber_id]["callback_function"]
+        r = redis.Redis()
+        self.subscribers[subscriber_id]["redis_pubsub"].subscribe(**{channel_id: callback_function})
+
+    def decrypt(self, x):
+        return str(x.decode("ascii")) if type(x) == bytes else x 
+
+    def publish(self, channel_id, message):
+        self.redis_main.publish(self.decrypt(channel_id), str(message).encode('ascii'))
+
+    def start(self):
+        def start_subscriber(subscriber_id):
+            while True:
+                message = self.subscribers[subscriber_id]["redis_pubsub"].get_message()
+                if message:
+                    print("Got message! in " + subscriber_id + str(message))
+                # else:
+                #     print("waiting")
+                #time.sleep(0.1)
+
+        for subscriber_id in self.subscribers:
+            process = Thread(target=start_subscriber,
+                    args=(subscriber_id,))
+            #process = Process(target=start_subscriber,
+            #        args=(subscriber_id,))
+            #process.daemon = True
+            process.start()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
