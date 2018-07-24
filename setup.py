978e860878eaee13b2d032e71f5a85b089ad1cfc
Andrew Tsai <andytsai14@gmail.com>
2018-07-23 21:47:37 -0700

    added package info

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='hms',
+      version='0.0.1',
+      description='factor graph framework for machine and reinforcement learning',
+      url='https://github.com/vramesh/factor-graph-compute',
+      author='Andrew Yunta Tsai, Vinayak Ramesh, Devavrat Shah, Suchan Vivatsethachai',
+      author_email='andytsai14@gmail.com',
+      license='MIT',
+      packages=setuptools.find_packages(),
+      zip_safe=False)
